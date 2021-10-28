67ac945683e47503817c265c8738b04c4ab6b0b0
bilgecakar <bilgecakar123@outlook.com>
2021-10-28 21:29:11 +0300

    Add cheat/debug keys

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -14,16 +14,35 @@ public class CollisionHandler : MonoBehaviour
     AudioSource audioSource;
 
     bool isTransitioning=false;
+    bool collisiopnDisable = false;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
+    private void Update()
+    {
+        RespondToDebugKeys();
+    }
+
+    void RespondToDebugKeys()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if(Input.GetKeyDown(KeyCode.C))
+        {
+            collisiopnDisable = !collisiopnDisable; //toggle collsion
+
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
 
-        if(isTransitioning==true) { return; }
+        if(isTransitioning || collisiopnDisable) { return; }
 
         switch(collision.gameObject.tag)
         {
